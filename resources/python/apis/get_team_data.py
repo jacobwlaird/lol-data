6e18859093b6dfdab4e6b9babfeaf97b27559ca0
Paul <spaynkee@gmail.com>
2020-08-05 21:36:10 -0500

    made some small changes to src/App.js

--- resources/python/apis/get_team_data.py
+++ resources/python/apis/get_team_data.py
@@ -38,8 +38,7 @@ def get_team_data(prod=True):
         print(json_data) 
 
 def main():
-
-	return get_teamo_data(sys.argv[1])
+	return get_team_data(sys.argv[1])
 
 if __name__ == "__main__":
 	main()
--- src/App.js
+++ src/App.js
@@ -2,36 +2,69 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-
-	const [currentTime, setCurrentTime] = useState(0);
+function Table(props) {
+	const [teamData, setTeamData] = useState(0);
 
 	useEffect(() => {
-	fetch('/react_test').then(res => res.json()).then(data => {
-		    setCurrentTime(data.message);
+	fetch('/api/get_team_data').then(res => res.json()).then(data => {
+		    setTeamData(data);
 		  });
 	}, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
-	  <p>The server message is {currentTime}.</p>
-      </header>
-    </div>
-  );
+//	{teamData.map(game => (
+//		<tr key={game.match_id}>
+//			<td>ok: {game.match_id}</td>
+//			<td>{game.participants}</td>
+//		</tr>
+//	))}
+	return (<table>
+
+			<tr>
+				<th>Match_id</th>
+				<th>UglyJson</th>
+			</tr>
+			<tr>
+				<td> td1 </td>
+				<td> td2 </td>
+			</tr>
+
+		</table>
+	);
+}
+
+function App() {
+
+	const root = (
+		<div className="App" id="root">
+		<Table />
+		</div>
+	);
+
+
+	return root
+
+// move the old stuff for now.
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//
+//	  <p>The server message is {currentTime}.</p>
+//      </header>
+//    </div>
+//  );
 }
 
 export default App;
