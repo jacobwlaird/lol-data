1171f73d67114a8bb07f18dce00b87c3d36f4491
Paul <spaynkee@gmail.com>
2020-09-16 20:11:59 -0500

    missed a second script

--- resources/python/scripts/perk_table_setup.py
+++ resources/python/scripts/perk_table_setup.py
@@ -1,18 +1,30 @@
-import json
-import os
-import os.path
-import time
-from sqlalchemy import Column, Table, Integer, String, BigInteger, Boolean, Float, TIMESTAMP, Time, MetaData
+""" perks_table_setup
+
+    This small script drops and creates the perks table in case we need to
+    reset this table, or get new data from totally empty.
+
+    Note: This, like all other scripts have to be ran from resources/python for now.
+
+    Also, this table doesn't exist, but we have this script anyway, when it actually does.
+
+"""
+from sqlalchemy import Column, Table, Integer, String, MetaData
 from classes.lolparser import LolParser
 
-# drop the perks table 
-perks_table = Table('perks', LolParser.metadata, autoload=True, autoload_with=LolParser.engine)
-perks_table.drop(LolParser.engine)
+def main():
+    """ main function of perks_table_setup import from LolParser and then creates the table
 
-# create the perks table
-perks_table = Table('perks',
-        MetaData(),
-        Column('key', Integer, primary_key=True),
-        Column('name', String(40)))
+    """
+    # drop the perks table
+    perks_table = Table('perks', LolParser.metadata, autoload=True, autoload_with=LolParser.engine)
+    perks_table.drop(LolParser.engine)
 
-perks_table.create(LolParser.engine)
+    # create the perks table
+    perks_table = Table('perks',
+            MetaData(),
+            Column('key', Integer, primary_key=True),
+            Column('name', String(40)))
+
+    perks_table.create(LolParser.engine)
+
+main()
