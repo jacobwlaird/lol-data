462686c35965258ece7f1ea6dd37df487ba78543
Paul <spaynkee@gmail.com>
2020-09-16 20:08:35 -0500

    missed a script during my linting

--- resources/python/scripts/json_table_setup.py
+++ resources/python/scripts/json_table_setup.py
@@ -1,26 +1,37 @@
-import json
-import os
-import os.path
-import time
+""" json_table_setup
+
+    This small script drops and creates the json_data table in case we need to
+    reset this table, or get new data from totally empty.
+
+    Note: This, like all other scripts have to be ran from resources/python for now.
+
+"""
 from sqlalchemy import Column, Table, BigInteger, MetaData, Text
 from classes.lolparser import LolParser
 
-# drop the json table 
-try:
-    json_table = Table('json_data', LolParser.metadata, autoload=True, autoload_with=LolParser.engine)
-    json_table.drop(LolParser.engine)
-except:
-    print("Json table didn't exist, probably, so we'll just create it now")
-    
+def main():
+    """ main function of json_table_setup import from LolParser and then creates the table
 
-# create the json table
-try:
-    json_table = Table('json_data',
-            MetaData(),
-            Column('match_id', BigInteger, primary_key=True),
-            Column('json_data', Text))
+    """
+    # drop the json table
+    try:
+        json_table = Table('json_data', LolParser.metadata,\
+                autoload=True, autoload_with=LolParser.engine)
+        json_table.drop(LolParser.engine)
+    except:
+        print("Json table didn't exist, probably, so we'll just create it now")
 
-    json_table.create(LolParser.engine)
-except Exception as e:
-    print(e)
-    print("Create json_data failed, better figure out why.")
+
+    # create the json table
+    try:
+        json_table = Table('json_data',
+                MetaData(),
+                Column('match_id', BigInteger, primary_key=True),
+                Column('json_data', Text))
+
+        json_table.create(LolParser.engine)
+    except Exception as e:
+        print(e)
+        print("Create json_data failed, better figure out why.")
+
+main()
