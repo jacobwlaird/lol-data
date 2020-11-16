e6c0882edd318a9687a1fe2c6335f1ab552a18f8
Paul <spaynkee@gmail.com>
2020-11-15 22:09:18 -0600

    fixing git ignore, I hope

--- src/apis/update_data.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-
-pub fn update_data() -> std::string::String {
-
-    Command::new("python3").arg("./resources/python/loldata.py").arg("ManualWeb").spawn().expect("It failed I guess?");
-
-    return "{\"message\":\"okay\"}".to_string()
-
-}
